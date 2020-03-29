2712bfad4832600ccee8c4e9737d4b0e7d44c2e1
Egor Litvinchuk <egor.litvinchuk@gmail.com>
2020-03-29 21:31:34 +0300

    check

--- .github/actions/validate-application/entrypoint.sh
+++ .github/actions/validate-application/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -l
 
-echo "First $1"
-echo "Second $2"
-echo "Second $application"
-
-yarn workspace $1 test
+echo "yarn workspace $1 test"
+yarn workspace "$1" test
 time=$(date)
 echo "::set-output name=time::$time"
