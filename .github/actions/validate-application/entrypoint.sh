7981639d5cf518592681aecadea0e526fc160d3b
Egor Litvinchuk <egor.litvinchuk@gmail.com>
2020-03-30 18:57:29 +0300

    check

--- .github/actions/validate-application/entrypoint.sh
+++ .github/actions/validate-application/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -l
 
-#echo "yarn workspace $1 test"
+# Installing dependencies
+yarn install --frozen-lockfile
+
 yarn workspace "$1" test
+
+git_hash=$(git rev-parse --short HEAD)
+
+echo "$git_hash"
+
 time=$(date)
 echo "::set-output name=time::$time"
